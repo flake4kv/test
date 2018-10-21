a3cce9d401796d1ec11df2cb71f5ac2cb12bdc4f
stevan <andric.stevan@yhahoo.com>
2018-10-21 15:44:54 +0200

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{config('APP_NAME', 'Stevan')}}</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>{{$title}}</h1>
+<p>This is the Laravel application from the "Laravel from scratch Youtube series."</p>
+@endsection
